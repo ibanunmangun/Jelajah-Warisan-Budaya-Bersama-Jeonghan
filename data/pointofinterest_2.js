a3dced120bdac30be9364b98ceeddbe61ab0b42a
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-21 20:18:03 +0800

    Add files via upload

---
+++ data/pointofinterest_2.js
@@ -0,0 +1,12 @@
+var json_pointofinterest_2 = {
+"type": "FeatureCollection",
+"name": "pointofinterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Malioboro Jogjakarta", "desc": "MALIOBORO JOGJAKARTA, Jl. Malioboro, Sosromenduran, Gedong Tengen, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55271, Indonesia", "X": "110.365843", "Y": "-7.7925764", "No": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 110.365843, -7.7925764 ] } },
+{ "type": "Feature", "properties": { "name": "Poka Ribs", "desc": "Poka Ribs, Tirtodipuran St No.20, Mantrijeron, Yogyakarta City, Special Region of Yogyakarta 55143, Indonesia", "X": "110.364150", "Y": "-7.8185266", "No": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 110.3641507, -7.8185266 ] } },
+{ "type": "Feature", "properties": { "name": "Tempo Gelato Prawirotaman", "desc": "Tempo Gelato Prawirotaman, Jl. Prawirotaman No.38B, Brontokusuman, Mergangsan, Yogyakarta City, Special Region of Yogyakarta 55153, Indonesia", "X": "110.372547", "Y": "-7.8195895", "No": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 110.3725478, -7.8195895 ] } },
+{ "type": "Feature", "properties": { "name": "Candi Borobudur", "desc": "Borobudur Temple, Jl. Badrawati, Kw. Candi Borobudur, Borobudur, Magelang Regency, Central Java, Indonesia", "X": "110.203751", "Y": "-7.6078738", "No": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 110.2037513, -7.6078738 ] } },
+{ "type": "Feature", "properties": { "name": "Sasanti Restaurant", "desc": "Sasanti Restaurant, Jl. Palagan Tentara Pelajar No.52, Panggung Sari, Sariharjo, Ngaglik, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": "110.373591", "Y": "-7.741969", "No": 5.0 }, "geometry": { "type": "Point", "coordinates": [ 110.3735913, -7.741969 ] } }
+]
+}
